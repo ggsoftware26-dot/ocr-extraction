8fd500046b88d7122b9cb93098aa5c1662e28c4f
Eliel Gez <eliel7114@gmail.com>
2026-08-20 00:15:34 +0300

    views upload sample

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import './load-env';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'node:path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const viewsPath =
+    process.env.NODE_ENV === 'production'
+      ? join(__dirname, 'views')
+      : join(process.cwd(), 'src', 'views');
+  app.useStaticAssets(viewsPath, { prefix: '/views' });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
